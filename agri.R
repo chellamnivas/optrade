91787027cd1bb43a3191c4c7ccf12163f507b934
chellamnivas <chellamnivas@gmail.com>
2020-01-08 18:53:13 +0530

    Plain Selling without buying

--- agri.R
+++ agri.R
@@ -5,8 +5,9 @@ require('stringr',warn.conflicts = T);
 all_day_data<-mongo(collection = 'options',db = 'upstroke',url = 'mongodb://localhost:27017',verbose = T);
 prev_dates_frame=all_day_data$aggregate('[{"$group": {"_id": "$date"}}, {"$addFields": {"_stamp": {"$dateFromString":{"dateString":"$_id","format":"%d/%m/%Y"}}}}, {"$sort": { "_stamp": -1 }},{"$project":{ "date":1}}]');
 req_dates=prev_dates_frame$`_id`[1:1];
-margin_data<<-data.frame(date=c(0),symbol=c(0),open=c(0),sl=c(0),slt=c(0),slh=c(0),close=c(0),margin=c(0),stringsAsFactors = FALSE);
+margin_data<<-data.frame(date=c(0),index=c(0),symbol=c(0),open=c(0),sl=c(0),slt=c(0),slh=c(0),close=c(0),lot=c(0),margin=c(0),stringsAsFactors = FALSE);
 
+index_data<<-data.frame(lot_size=c(75,20),row.names = c("NIFTY","BANKNIFTY"))
 aggreOutput<-lapply(req_dates,function(l){
-  singleDay(l,"9:15AM",all_day_data,"3:10PM")
+  singleDay(all_day_data,l,"9:15AM","3:10PM",7,4)
 })
--- singleDay.R
+++ singleDay.R
@@ -1,4 +1,4 @@
-singleDay<-function(date,entry,data,exit){
+singleDay<-function(data,date,entry,exit,ce,pe){
   single_day_data<-data$find(paste('{"date":"',date,'"}',sep = ''));
   entry_string<-paste(date,entry,sep=" ");
   entrytimems<-as.numeric(as.POSIXct(entry_string,format="%d/%m/%Y %I:%M%p"))*1000;
@@ -14,24 +14,33 @@ singleDay<-function(date,entry,data,exit){
       })(entryCandles$symbol)),
     type=((function(x){str_sub(x,-2)})(entryCandles$symbol)),
   )%>%arrange(type,index,strike)
+   entrySymbols<-entryCandles%>%group_by(index,type)%>%mutate(rank=rank(as.numeric(strike)));
+   entryTokens<-entrySymbols%>%filter((type=="CE"&rank==ce) | (type=="PE"&rank==pe));
    
-  apply(entryCandles,1,function(token){
-      
-    token_symbol<-token['symbol'];print(token_symbol);
-    open_price<-as.numeric(token['open']);print(open_price);
+  apply(entryTokens,1,function(token){
+    
+    token_symbol<-token['symbol'];
+    open_price<-as.numeric(token['open']);
     single_day_symbol_data<-single_day_data%>%filter(symbol== token_symbol,timestamp<=exittimems);
     stop_loss_candles<-single_day_symbol_data%>%filter(high > (open_price*1.5))%>%arrange(timestamp);
     exit_candle<-single_day_symbol_data%>%filter(timestamp ==exittimems);
     exit_price<-exit_candle$close;
+    
+    contra_open<-(entryTokens%>%filter(as.numeric(open) != open_price & index==token['index']))$open;
+    
+    if(open_price > contra_open){
+      lot<-1;
+    }
+    if(open_price<contra_open){
+      lot<-round(contra_open/open_price)
+    }
+    lot_size<-index_data[token['index'],]
     if(nrow(stop_loss_candles) > 0){
       first_stop_loss_candle<-stop_loss_candles[1,];
-      margin_data<<-rbind(margin_data,c(date=date,symbol=token_symbol,open=open_price,sl=T,slt=first_stop_loss_candle$time,slh=first_stop_loss_candle$high,close=exit_price,margin=-0.5))
-      
+      margin_data<<-rbind(margin_data,c(date=date,index=token['index'],symbol=token_symbol,open=open_price,sl=T,slt=first_stop_loss_candle$time,slh=first_stop_loss_candle$high,close=exit_price,lot=lot,margin=lot*lot_size*(open_price-(open_price*1.5))));
     }
     if(nrow(stop_loss_candles) < 1){
-      
-      margin_data<<-rbind(margin_data,c(date=date,symbol=token_symbol,open=open_price,sl=F,slt="",slh="",close=exit_price,margin=(open_price-exit_price)/open_price))
-      
+      margin_data<<-rbind(margin_data,c(date=date,index=token['index'],symbol=token_symbol,open=open_price,sl=F,slt="",slh="",close=exit_price,lot=lot,margin=lot*lot_size*((open_price-exit_price))));
     }
     
     })
